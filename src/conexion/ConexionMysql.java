bd78e47d23cbb0481919ecd57815de625f7e2018
Sergio Blancas Morales <sblancas748@gmail.com>
2024-11-06 00:03:22 -0600

    Mi primer commit

---
+++ src/conexion/ConexionMysql.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author sergi
+ */
+public class ConexionMysql {
+   Connection conexion;
+   public Connection conectar(){
+       try {
+           Class.forName("com.mysql.jdbc.Driver");
+           conexion=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/almacen","root","Foreach7");
+           System.out.println("Conexion exitosa");
+       }catch(Exception e){
+           System.err.println("Fallo la conexion"+e);
+       }
+       return conexion;
+   }
+  
+}
